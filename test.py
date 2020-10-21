dcebbac33590a2517963f9e8b2aeb81d294252d7
Nastya Rozhnova <nastyarozhnova@icloud.com>
2020-10-21 15:43:30 +0300

    111

--- test.py
+++ test.py
@@ -1,10 +1,8 @@
-print('hello')  # string(строка, текст) str(2) -> '2'
-print (123)  # integer (целое число) int('2') -> 2
-
+print('hello')
+print (123)
 name='Настя'
 print(name)
 a=int(input('Введите первое число\n'))
 b=int(input('Введите второе число\n'))
 s=a+b-a
 print('Ответ',s)
-
