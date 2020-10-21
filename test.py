7dd9cd0bf849a21f5c01b636a1885a3617ab4ea7
lyubomyr83 <lyubomyr83@mail.ru>
2020-10-21 15:34:29 +0300

    git

--- test.py
+++ test.py
@@ -6,4 +6,5 @@ a=int(input('Введите первое число\n'))
 b=int(input('Введите второе число\n'))
 s=a+b-a
 print('Ответ',s)
-
+git config  --global user.name "Nastya Rozhnova" 
+git config  --global user.email nastyarozhnova@icloud.com
